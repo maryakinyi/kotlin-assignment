d4033147ac16ccb573d2a61f53b97c4fb6cc92d4
maryakinyi <owuorakinyimary@gmail.com>
2021-02-20 23:29:14 +0300

    first assignment

--- src/hello.kt
+++ src/hello.kt
@@ -1,4 +1,14 @@
 fun main() {
-    var name:String="mary"
-    print (name)
-}
+    val name:String="Sarah Papai"
+    var age=19
+    val phoneNumber="0702128264"
+    val weight="65"+ "kgs"
+    val nationality:String="kenyan"
+    val kenyan=true
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(nationality)
+    println(kenyan)
+    }
